e0cf25f6e94a999ba0a15ccd7098bf800ca374f5
gmsantos <gabriel.ms1@hotmail.com>
2020-01-09 12:42:45 -0300

    Create an application setting

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,20 +6,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AspNetMvc.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace AspNetMvc.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration _configuration;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
+            _configuration = configuration;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
+            ViewBag.Environment = _configuration.GetValue<string>("Environment");
             return View();
         }
 
