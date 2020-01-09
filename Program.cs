42be21476abd8bc128b819c7c05c0a01130ffd16
gmsantos <gabriel.ms1@hotmail.com>
2020-01-09 12:16:47 -0300

    Implement custom configuration loader for Beanstalk

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TTRider.Aws.Beanstalk.Configuration;
 
 namespace AspNetMvc
 {
@@ -18,6 +19,10 @@ namespace AspNetMvc
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddBeanstalkParameters();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
